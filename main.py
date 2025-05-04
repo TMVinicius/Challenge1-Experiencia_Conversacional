75841dc798f27fe173f270d0ccde357d0c743d32
TMVinicius <viniciustavaresbr@gmail.com>
2025-05-04 20:41:59 -0300

    Add /partidas command to main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import telebot
 from config import CHAVE_API
-from utils.elenco import mensagem_elenco
-from utils.rating import mensagem_rating
-from utils.constants import text_base, text_menu
+from utils.elenco import message_elenco
+from utils.rating import message_rating
+from utils.constants import text_base, text_menu, ORG
+from utils.loja import message_loja
+from utils.produtos import message_produtos
+from utils.quiz import sortear_perguntas, message_quiz
+from services.scraping_partidas import ScrapingPartidas
 
 bot = telebot.TeleBot(CHAVE_API)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    text = "Olá! Eu sou o bot Furia CS. Para começar, me informe o que você deseja fazer:"
+    text = f"Olá! Eu sou o bot {ORG} CS 🐾 Para começar, me informe o que você deseja fazer:"
     text += text_base
     bot.send_message(message.chat.id, text)
 
@@ -16,7 +20,7 @@ def start(message):
 @bot.message_handler(commands=['elenco'])
 def elenco_handler(message):
     try:
-        text = mensagem_elenco()
+        text = message_elenco()
         bot.send_message(message.chat.id, text, parse_mode="Markdown")
     except Exception as e:
         bot.send_message(message.chat.id, f"Erro ao buscar elenco: {str(e)}")
@@ -25,34 +29,68 @@ def elenco_handler(message):
 @bot.message_handler(commands=['rating'])
 def rating_handler(message):
     try:
-        text = mensagem_rating()
+        text = message_rating()
         bot.send_message(message.chat.id, text, parse_mode="Markdown")
     except Exception as e:
         bot.send_message(message.chat.id, f"Erro ao buscar ratings: {str(e)}")
 
+scraper = ScrapingPartidas()
+
 @bot.message_handler(commands=['partidas'])
-def start(message):
-    text = "Aqui voce receberá alertas de partidas ao vivo!"
-    bot.send_message(message.chat.id, text)
+def partidas_handler(message):
+    bot.send_message(message.chat.id, f"🔍 Buscando próxima partida da {ORG}...")
+    info_partida = scraper.get_partida()
+    bot.send_message(message.chat.id, info_partida, parse_mode='Markdown')
+    menu_text = "👇  Aqui estão os comandos que você pode usar:\n"
+    menu_text += text_base
+    bot.send_message(message.chat.id, menu_text)
 
 
 @bot.message_handler(commands=['loja'])
-def start(message):
-    text = "Aqui vai o loja"
-    bot.send_message(message.chat.id, text)
+def loja_handler(message):
+    try:
+        imagem_url, legenda = message_loja()
+        legenda += "\n" + text_menu
+        if imagem_url:
+            bot.send_photo(message.chat.id, photo=imagem_url, caption=legenda, parse_mode="Markdown")
+        else:
+            bot.send_message(message.chat.id, legenda)
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Erro ao buscar produto: {str(e)}")
 
 
+@bot.message_handler(commands=['produtos'])
+def produtos_handler(message):
+    try:
+        lista_produtos = message_produtos()
+        for imagem_url, legenda in lista_produtos:
+            bot.send_photo(message.chat.id, photo=imagem_url, caption=legenda, parse_mode="Markdown")
+        
+        menu_text = "👇  Aqui estão os comandos que você pode usar:\n"
+        menu_text += text_base
+        bot.send_message(message.chat.id, menu_text)
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Erro ao enviar produtos: {str(e)}")
+
+
+usuarios_quiz = {}
 @bot.message_handler(commands=['quiz'])
-def start(message):
-    text = "Aqui vai o quiz"
-    bot.send_message(message.chat.id, text)
+def quiz_handler(message):
+    user_id = message.from_user.id
+    perguntas = sortear_perguntas()
 
+    usuarios_quiz[user_id] = {"perguntas": perguntas, "indice": 0, "acertos": 0}
 
-@bot.message_handler(commands=['menu'])
-def start(message):
-    text = "Aqui estão os comandos que você pode usar:\n"
-    text += text_base
-    bot.send_message(message.chat.id, text)
+    primeira_pergunta = perguntas[0]
+    texto_pergunta = f"{primeira_pergunta['pergunta']}\n"
+    for opcao in primeira_pergunta['opcoes']:
+        texto_pergunta += opcao + "\n"
+
+    bot.send_message(message.chat.id, texto_pergunta)
+
+@bot.message_handler(func=lambda message: message.from_user.id in usuarios_quiz)
+def handle_resposta_quiz(message):
+    message_quiz(bot, message, usuarios_quiz)
 
 
 
